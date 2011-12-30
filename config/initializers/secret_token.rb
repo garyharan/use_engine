81db97b123f4b8f4d7d97d3cfaac2e41d3119e23
Gary Haran <gary@openplaces.com>
2011-12-30 13:12:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UseEngine::Application.config.secret_token = '483959a9cbd58e0e0d1a83c424f3ed8975ac683fd60724acdd984fc3dafea72130740a54364a467a3075694d10e0f0c43d4afc100719cb61362b8e5b74d80320'
